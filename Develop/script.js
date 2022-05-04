dfe3a90240dd8840b93cc29d8b20ea5deef8e73e
Ben <byeungis@gmail.com>
2022-05-04 01:16:42 -0400

    updated file

--- Develop/script.js
+++ Develop/script.js
@@ -1,70 +1,68 @@
-// Assignment code here  
-function getRandom(max){  //for random numbers and characters
-  var random = Math.random() * max;
-  return Math.floor(random) ;
-}
+//Assignment code here  
+var lowercaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] 
+var uppercaseList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var numberList = ["0","1","2","3","4","5","6","7","8","9"]
+var symbolList = ["~","`","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}","|",";","'",":","<",".",">","/","?"]
+var promptInput = ""
 
+//creates the value and the only section that should be writtens
+function generatePassword(){  
 
-
-
-function generatePassword(){  //creates the value and the only section that should be writtens
-  
+  //stores the password
+   var generatePassword = ""
    
+
   // a. password length 8 < 128
   var promptInput= window.prompt('How many characters would you like the password to contain?  Please note: Minimum 8 and Max 128.');
  
-  var passwordLength = parseInt(promptInput) //find a way to make into a number
-   
-  //validate prompt answer
 
-  if(isNaN(passwordLength) || passwordLength ===""|| passwordLength ===null ||passwordLength <8 && passwordLength >128){  //if entered a non-numeric, empty, less than 8, more than 128, will not generate.
-    window.alert("Please provide a numeric value between 8 & 128. Try again")
-    return generatePassword();
+ if(promptInput >= 8 && promptInput <=128){
+   passwordLength = promptInput;
+
+   // a. Lowercases
+    var promptLowercase = window.confirm("Would you like to include lowercase?");
+    // b. Uppercase
+    var promptUppercase = window.confirm("Would you like to include uppercase?");
+    // c. Numbers
+    var promptNumbers = window.confirm("Would you like to include numbers?");
+    // d. special characters
+    var promptSpecialChar = window.confirm("Would you like to include special characters?");
+  } else{
+    window.alert("Please provide a numeric value. Between 8 & 128. Try again.");
+    return promptInput;
   }
 
+  var passwordCart = []
 
-  // b. Lowercase
-  var promptLowercase = window.confirm("Would you like to include lowercase?")
-  if (promptLowercase ===true){
-    passwordCart += lcase;
+ if (promptLowercase ===true){
+    passwordCart.push(lowercaseList)
   }
-  // c. Uppercase
-  var promptUppercase = window.confirm("Would you like to include uppercase?")
-  if (promptUppercase ===true){
-    passwordCart += ucase;
+ if (promptUppercase ===true){
+    passwordCart.push(uppercaseList)
   }
-
-  // d. Numbers
-  var promptNumbers = window.confirm("Would you like to include numbers?")
-  if (promptNumbers ===true){
-      passwordCart += numbers;
-    }
-
-
-  // e. special characters
-  var promptSpecialChar = window.confirm("Would you like to include special characters?")
-  if (promptSpecialChar ===true){
-    passwordCart += char;
+ if (promptNumbers ===true){
+    passwordCart.push(numberList)
+  }
+ if (promptSpecialChar ===true){
+    passwordCart.push(symbolList)
   }
 
-  //generate password based on critera
+ 
 
-  for (var i = 0; i< passwordLength; i++){
-    generatePassword +=passwordCart[Math.floor(Math.random() * passwordCart.length)];
+  for (var i = 0; i < passwordLength; i++){
+    generatePassword = passwordCart[Math.floor(Math.random() * passwordCart.length)];
   }
 
+ 
 
-
-
-
-  //display password to the page
-  return generatePassword//generated password will go here but write something afterwards
+  //display password to the page, generated password will go here but write something afterwards
+  return generatePassword 
 }
 
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");  //targest the generate button, no need to touch, line 28
+var generateBtn = document.querySelector("#generate");  //targets the generate button, no need to touch, line 28
 
 
 // Write password to the #password input
@@ -80,3 +78,11 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);  //listen to the button on the click and write the password, no need to touch
 
 
+   //only inputs number
+  //var passwordLength = parseInt(promptInput) 
+   
+  //if entered a non-numeric, empty, less than 8, more than 128, will not generate.
+  // if(isNaN(passwordLength) || passwordLength ===""|| passwordLength === null || passwordLength <8 || passwordLength > 128){
+  //   window.alert("Please provide a numeric value. Between 8 & 128. Try again.")
+  //   return
+  // } 
